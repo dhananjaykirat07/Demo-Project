451cb0d4ad598af9981449cd2571b07af2b81685
dhananjaykirat07 <dhananjaykirat07@gmail.com>
2024-12-16 14:18:35 +0530

    Automated Story_2024

---
+++ src/test/java/com/practiceSD/Hello.java
@@ -0,0 +1,11 @@
+package com.practiceSD;
+
+public class Hello {
+
+	public static void main(String[] args) {
+	
+		System.out.println(" Hello, Madam__Ji. I am in the Love with U.💖❤️💕.");
+
+	}
+
+}
